07b10e16279b532bbe4e4864255a4d3f477cb532
本村陵太 <honsonryoufutoshi@honsonryoufutoshinoMacBook-Pro.local>
2021-03-18 23:18:13 +0900

    カウンター

---
+++ Programming.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Programming{
+    public static void methodA(){
+        String str = "Dou you like writing progamming?";
+        System.out.println(str.length());
+        methodB();
+    }
+    public static void methodB(){
+        List<Integer> points = new ArrayList<Integer>();
+        points.add(4);
+        System.out.println(points.get);
+    }
+    public static void main(String[] args){
+        methodA();
+    }
+}
